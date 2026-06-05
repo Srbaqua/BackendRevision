40b67cc2d8c24e2526e31efdad80e3a627cb57c2
Srbaqua <23bcs101@nith.ac.in>
2026-06-05 18:54:12 +0530

    id{10000}- Index.html removed and we work using node.js now

--- Readme.md
+++ Readme.md
@@ -4,11 +4,26 @@
 
 ## Introduction
 
-We will create Simple application autohrization authentication and other backend functionality using MERN Stack.
+We will create Simple application authored in Node.js and MongoDB.
 
 ## Prerequisites
 
 - Node.js
 - NPM
 - MongoDB
+
+
+## Notes For revision
+    Here we will notedown the impotant points which we need to remember while revising the backend development.
+    Along with that I put time stamps for each point.
+    and that give you clearity why we create any file or folder or remove it.
+ - **Note:**
+   - We will create a folder named **Backend** in the **D drive** and we will create a file named **Readme.md** in that folder.
+   - We created Index.html and Script.js in the Backend folder.
+   - We uses basic functions like forEach, map, filter, find, freeze, indexof.
+   - We uses Objects and other things like async, await.
+   
+   **
+   - Now we removed the index.html file as from now on we will be using Node.js to create the backend.
+
 
--- script.js
+++ script.js
@@ -1,30 +1,62 @@
 var arr = ['.',"..","...",1,2,3,7,5,3,"....",9,12," Saurabh"];
 
 
-//for each
-console.log("For each function");
-arr.forEach(function(val){
-    console.log("Hi"+ val);
-})
+// //for each
+// console.log("For each function");
+// arr.forEach(function(val){
+//     console.log("Hi"+ val);
+// })
 
-//map
-var newarr = arr.map(function(val){
-    return "Hi"+val;
-})
+// //map
+// var newarr = arr.map(function(val){
+//     return "Hi"+val;
+// })
 
-console.log("map function "+newarr);
+// console.log("map function "+newarr);
 
-//filter
-var filArr = arr.filter(function(val){
-    if(val>3)return true;
-    else return false;
-})
-console.log("Filter function "+filArr);
+// //filter
+// var filArr = arr.filter(function(val){
+//     if(val>3)return true;
+//     else return false;
+// })
+// console.log("Filter function "+filArr);
 
 
-//find ->give the first element which satisfy the condition
+// //find ->give the first element which satisfy the condition
 
-var find =  arr.find(function(val){
-    if(val>3)return true;
-})
-console.log("Find function "+find);
+// var find =  arr.find(function(val){
+//     if(val>3)return true;
+// })
+// console.log("Find function "+find);
+ 
+
+//Objects
+var obj =  {
+    name:"Saurabh",
+    age:25,
+    address:"x",
+    something:1,
+};
+//to access the object
+ obj.name;
+ obj["name"];
+//  Object.freeze(obj);// freeze the object
+ obj.name = "Saurabh Kumar";
+
+
+function x(){
+    console.log("1");
+}
+async function y(){
+     console.log("2");
+}
+
+function z(){
+    console.log("3");
+}
+function a(){
+    x();
+    y();
+    z();
+}
+a();
