475c1c8947906fb77e387d58572e8bdd74ae7ccd
Srbaqua <23bcs101@nith.ac.in>
2026-06-06 02:43:24 +0530

    Learn Http

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
-// ```basic we study-
-//         writefile
-//         readfile
-//         appendfile
-//         deletefile
-//         copyfile
-//         renamefile
-//         unlinkfile    
-// ```
-const fs =  require('fs');
-// fs.writeFile('hey.txt',"Hello checking writeFile",function(err){
-//     if(err){console.log(err)}
-//     else{
-//         console.log("File written successfully");
-//     }  
-// })
-// fs.appendFile("hey.txt","\n lets add something more to check append file functionality",function(err){
-//     if(err){
-//         console.log(err);
-//     }else{
-//         console.log("Append successfully");
-//     }
-// })
-// fs.rename("hey.txt","changeNamed.txt",function(err){
-//     if(err)console.log(err);
-//     else{
-//         console.log("Renamed Successfully");
-//     }
-// })
-fs.readFile("changeNamed.txt","utf8",function(err,data){
-    if(err)console.log(err);
-    else{
-        console.log("file read \n",data);
-    }
+const http  = require('http');
+const server = http.createServer(function(req,res){
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.end("Helloo I am working");
 })
+
+server.listen(3000);
