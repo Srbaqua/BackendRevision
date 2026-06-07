cdb7cb8453a0ee63ba20b2a2a7dc7a5bbe10bd5f
Srbaqua <23bcs101@nith.ac.in>
2026-06-08 00:31:33 +0530

    Learnt ejs and dynamic routing

--- LearnWithPractice/index.js
+++ LearnWithPractice/index.js
@@ -1,2 +1,18 @@
 const express = require('express');
+const path = require('path')
+
+
 const app  = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(path.join(__dirname,"public")));
+app.set('view engine','ejs');
+
+app.get("/:name",function(req,res){
+    var name = req.params.name;
+    res.send(`I am ${name}`) 
+})
+
+app.listen(3000);
