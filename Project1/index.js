fd3534070ff0151e88a5fb55eb0e6a3e50dcc660
Srbaqua <23bcs101@nith.ac.in>
2026-06-10 03:02:39 +0530

    completed server logic for creation of file and reading a already created file

--- Project1/index.js
+++ Project1/index.js
@@ -1,18 +1,41 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const path = require('path');
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"public")));
-app.set("view engine",'ejs');
 
-app.get("/Profile",function(req,res){
-    res.render("index");
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", 'ejs')
+
+
+app.get('/', function (req, res) {
+    fs.readdir("./files", function (err, files) {
+        res.render('index', { files: files });
+    })
 })
-app.get("/Profile/username/:name",function(req,res){
-    res.send(`<h1>Welcome ${req.params.name}</h1>`);
+
+app.post('/create', function (req, res) {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.content,function(err){
+        if(err)console.log(err);
+        res.redirect('/');
+    });
 })
+
+
+app.get('/files/:filename', function(req, res) {
+    console.log("Requested:", req.params.filename);
+    fs.readFile(`./files/${req.params.filename}.txt`, "utf-8", function(err, filedata) {
+        if (err) {
+            console.log(err);
+            return res.status(404).send("File not found");
+        }
+        console.log(filedata);
+        res.render('show',{filename:req.params.filename,filedata:filedata});
+    })
+})
+
 
 app.listen(3000);
