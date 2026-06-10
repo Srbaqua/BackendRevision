3403774f5170b5fe8215a05e6aba9380c110b2cd
Srbaqua <23bcs101@nith.ac.in>
2026-06-11 01:41:07 +0530

    Edit option added

--- Project1/index.js
+++ Project1/index.js
@@ -18,24 +18,36 @@ app.get('/', function (req, res) {
 })
 
 app.post('/create', function (req, res) {
-    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.content,function(err){
-        if(err)console.log(err);
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.content, function (err) {
+        if (err) console.log(err);
         res.redirect('/');
     });
 })
 
 
-app.get('/files/:filename', function(req, res) {
+app.get('/files/:filename', function (req, res) {
     console.log("Requested:", req.params.filename);
-    fs.readFile(`./files/${req.params.filename}.txt`, "utf-8", function(err, filedata) {
+    fs.readFile(`./files/${req.params.filename}.txt`, "utf-8", function (err, filedata) {
         if (err) {
             console.log(err);
             return res.status(404).send("File not found");
         }
         console.log(filedata);
-        res.render('show',{filename:req.params.filename,filedata:filedata});
+        res.render('show', { filename: req.params.filename, filedata: filedata });
     })
 })
 
+app.post('/edit', function (req, res) {
+    fs.rename(`./files/${req.body.oldname}.txt`, `./files/${req.body.newname}.txt`, function (err) {
+        console.log(err);
+    })
+    return res.redirect('/');
+})
+
+app.get('/edit/:filename', function (req, res) {
+    res.render('edit', {
+        filename: req.params.filename
+    });
+});
 
 app.listen(3000);
